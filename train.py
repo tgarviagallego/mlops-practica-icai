9231044135b5f01aa67c24672bc45238eb5a4f6a
tgarviagallego <tgarviagallego@alu.comillas.edu>
2026-09-12 13:41:17 +0200

    Configure MLflow with DagsHub

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ from sklearn.metrics import accuracy_score
 import joblib 
 import mlflow 
 import mlflow.sklearn 
+import dagshub
+
+dagshub.init(repo_owner='202105183', repo_name='mlops-practica-icai', mlflow=True)
  
 # Cargar el conjunto de datos 
 iris = datasets.load_iris() 
@@ -20,7 +23,7 @@ with mlflow.start_run():
     ) 
  
     # Inicializar y entrenar el modelo 
-    model = RandomForestClassifier(n_estimators=100, random_state=42) 
+    model = RandomForestClassifier(n_estimators=200, random_state=42) 
     model.fit(X_train, y_train) 
  
     # Realizar predicciones y calcular la precisión 
@@ -34,7 +37,7 @@ with mlflow.start_run():
     mlflow.sklearn.log_model(model, "random-forest-model") 
  
     # Registrar parámetros y métricas 
-    mlflow.log_param("n_estimators", 100) 
+    mlflow.log_param("n_estimators", 200) 
     mlflow.log_metric("accuracy", accuracy) 
  
     print(f"Modelo entrenado y precisión: {accuracy:.4f}") 
